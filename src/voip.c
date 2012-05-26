6de6938e01a81bc30b8ade63b47fad298dd73b0d
Christopher De Sa <cdesa@stanford.edu>
2012-05-26 12:51:28 -0700

    Cut bitrate in half again.

--- src/voip.c
+++ src/voip.c
@@ -50,7 +50,7 @@ float distort(float x);
 float undistort(float x);
 float sgn(float x);
 
-const float mu = 127.0f;
+const float mu = 63.0f;
 
 QSAMPLE sample_to_qsample(SAMPLE x) {
   return (uint8_t)lroundf(mu*((distort(x)+1.0f)/2.0f));
